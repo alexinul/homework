07dfc4ef369f840d59e6313bdb1dd7c302cd5922
Alex <alexandrustana2@gmail.com>
2017-12-14 15:33:59 +0200

    WIP Type inference

--- hw02-type-inference/src/main/scala/Checker.scala
+++ hw02-type-inference/src/main/scala/Checker.scala
@@ -1,51 +1,51 @@
-import ast._
-import ast.Type
+import ast.{Type, _}
 import ast.operation._
 
-import scala.util.Random
-
 object Checker {
-
-  def apply(program: Expression, environment: Map[Expression, Type] = Map()): Type = {
-    program match {
-      case Const(_) => new IntType
-      case Bool(_) => new BoolType
+  def typeOf(exp: Expression, environment: Map[Expression, Type] = Map(), subst: Map[VarType, Type] = Map()): Answer = {
+    exp match {
+      case Const(_) => Answer(new IntType, subst)
+      case Bool(_) => Answer(new BoolType, subst)
       case ref@Val(_) => environment.get(ref) match {
-        case Some(value) => value
-        case None => throw new RuntimeException("Val was not declared")
+        case Some(value) => Answer(value, subst)
+        case None => throw new RuntimeException("Val not declared")
       }
-      case BinaryOperation(l, operation, r) => checkBinaryOperation(l, operation, r, environment)
-      case If(condition, ifThen, elseIf) => checkIf(condition, ifThen, elseIf, environment)
-      case Lambda(arguments, body, _) => checkLambda(arguments, body, environment)
-      case ValDecl(variable, body, _, _) => checkValDecl(variable, body, environment)
-      case Apply(lambda, parameters) => checkApply(lambda, parameters, environment)
-    }
-  }
+      case BinaryOperation(l, operation, r) => evaluateBinaryOperation(l, operation, r, subst, environment)
+      case If(condition, ifThen, elseIf) => {
+        val ans1 = typeOf(condition, environment, subst)
+        val subst1 = unifier(ans1.ty, new BoolType, ans1.subst, condition)
 
-  def checkBinaryOperation(l: Expression, operation: Operation, r: Expression, environment: Map[Expression, Type]): Type =
-    (apply(l, environment), apply(r, environment)) match {
-      case (IntType(), IntType()) => operation match {
-        case _: IntOperation => new IntType
-        case _: BoolOperation => new BoolType
+        val ans2 = typeOf(ifThen, environment, subst1)
+        val ans3 = typeOf(elseIf, environment, ans2.subst)
+
+        val subst2 = unifier(ans2.ty, ans3.ty, ans3.subst, exp)
+
+        Answer(ans2.ty, subst2)
       }
-      case _ => throw new RuntimeException("Binary Operations can be done only on integers")
+      case Lambda(arguments, body, returnType) => evaluateLambda(environment, subst, arguments, body, returnType)
+      case Apply(lambda, parameters) => {
+        val resultType = freshTvarType.getType()
+        val ans = typeOf(lambda, environment, subst)
+        val pAns = parameters.map { case (v, e) => v -> typeOf(e, environment, subst).ty }
+
+        unifier(ans.ty, FunctionType(pAns.values.toList, resultType), subst, exp)
+
+        new Answer(resultType, subst)
+      }
+      case ValDecl(variable, body, oVarTypes, oReturnType) => {
+        typeOf(body, environment ++ variable.map { case (v, e) => v -> typeOf(e, environment, subst).ty }, subst)
+
+        //        val variableTypes = variable.map { case (k, _) => k -> `oType->type`(oVarTypes.get(k).getOrElse(null)) }
+        //        val resultType = `oType->type`(oReturnType)
+        //
+        //        val f = new FunctionType(variableTypes.values.toList, resultType)
+        //        val ans = typeOf(body, environment ++ variableTypes, subst)
+        //
+        //        unifier(ans.ty, resultType, subst, body);
+        //
+        //        typeOf(body, environment ++ variable.map { case (k: Val, v: Expression) => (k, typeOf(v, environment, subst).ty) }, subst)
+      }
     }
-
-  def checkIf(condition: Expression, ifThen: Expression, elseIf: Expression, environment: Map[Expression, Type]): Type =
-    apply(condition, environment) match {
-      case BoolType() => if (apply(ifThen, environment) == apply(elseIf, environment)) apply(ifThen, environment) else throw new RuntimeException("If cannot have a different result type on each branch")
-      //TODO: improve " if (equalType(apply(ifThen), apply(elseIf))) apply(ifThen) "  I have to get rid of the third apply
-      case _ => throw new RuntimeException("If must have a boolean as the condition")
-    }
-
-  def checkLambda(arguments: Map[Val, Type], body: Expression, environment: Map[Expression, Type]): Type =
-    FunctionType(arguments.map(_._2).toList, apply(body, environment ++ arguments))
-
-  def checkValDecl(variable: Map[Val, Expression], body: Expression, environment: Map[Expression, Type]): Type =
-    apply(body, environment ++ variable.map { case (k: Val, v: Expression) => (k, apply(v, environment)) })
-
-  def checkApply(lambda: Expression, parameters: Map[Val, Expression], environment: Map[Expression, Type]): Type = {
-    apply(lambda, environment ++ parameters.map { case (k: Val, v: Expression) => (k, apply(v, environment)) })
   }
 
   def applyOneSubst(ty0: Type, tvar: VarType, ty1: Type): Type = {
@@ -56,22 +56,21 @@ object Checker {
     }
   }
 
-  def applySubstToType(ty: Type, subst: Map[Type, VarType]): Type = {
+  def applySubstToType(ty: Type, subst: Map[VarType, Type]): Type = {
     ty match {
       case self@(IntType() | BoolType()) => self
       case FunctionType(argumentsTypes, resultType) => FunctionType(argumentsTypes.map(argType => applySubstToType(argType, subst)), applySubstToType(resultType, subst))
-      case VarType(_) => subst.get(ty) match {
+      case self@VarType(_) => subst.get(self) match {
         case Some(value) => value
         case None => ty
       }
     }
   }
 
-  def emptySubst(): Map[Type, VarType] = Map.empty
+  def extendSubst(subst: Map[VarType, Type], tvar: VarType, ty: Type): Map[VarType, Type] = if (subst.size == 0) subst ++ Map(tvar -> ty)
+  else subst.map { case (k, v) => k -> applyOneSubst(v, tvar, ty) } ++ Map(tvar -> ty)
 
-  def extendSubst(subst: Map[Type, VarType], tvar: VarType, ty: Type): Map[Type, VarType] = subst.map { case (k, v) => (k, applyOneSubst(v, tvar, ty).asInstanceOf[VarType]) } + (ty -> tvar)
-
-  def unifier(ty1: Type, ty2: Type, subst: Map[Type, VarType], exp: Expression): Map[Type, VarType] = {
+  def unifier(ty1: Type, ty2: Type, subst: Map[VarType, Type], exp: Expression): Map[VarType, Type] = {
     val ty11 = applySubstToType(ty1, subst)
     val ty22 = applySubstToType(ty2, subst)
     if (ty11 == ty22) subst
@@ -87,7 +86,7 @@ object Checker {
     else if (ty11 == FunctionType && ty22 == FunctionType) {
       (ty11, ty22) match {
         case (FunctionType(argTy1, resTy1), FunctionType(argTy2, resTy2)) => {
-          (argTy1 zip argTy2).map { case (arg1, arg2) => unifier(arg1, arg2, subst, exp) }
+          (argTy1 zip argTy2).map { case (arg1, arg2) => subst ++ unifier(arg1, arg2, subst, exp) }
           unifier(resTy1, resTy2, subst, exp)
         }
       }
@@ -105,59 +104,24 @@ object Checker {
     }
   }
 
-  def typeOf(exp: Expression, environment: Map[Expression, Type] = Map(), subst: Map[Type, VarType] = Map()): Answer = {
-    exp match {
-      case Const(_) => Answer(new IntType, subst)
-      case Bool(_) => Answer(new BoolType, subst)
-      case ref@Val(_) => environment.get(ref) match {
-        case Some(value) => Answer(value, subst)
-        case None => Answer(freshTvarType.getType(), subst)
-      }
-      case BinaryOperation(l, operation, r) => evaluateBinaryOperation(l, operation, r, subst, environment)
-      case If(condition, ifThen, elseIf) => {
-        val ans1 = typeOf(condition, environment, subst)
-        unifier(ans1.ty, new BoolType, subst, condition)
+  private def evaluateLambda(environment: Map[Expression, Type], subst: Map[VarType, Type], arguments: Map[Val, Type], body: Expression, returnType: OptionalType) = {
+    val retType = `oType->type`(returnType)
+    val args = arguments.map { case (k, v) => (k, `oType->type`(v)) }
+    val ans = typeOf(body, environment ++ args, subst)
 
-        val ans2 = typeOf(ifThen, environment, subst)
-        val ans3 = typeOf(elseIf, environment, subst)
-
-        unifier(ans2.ty, ans3.ty, subst, exp)
-
-        Answer(ans2.ty, subst)
-      }
-      case Lambda(arguments, body, returnType) => {
-        val retType = `oType->type`(returnType)
-
-        typeOf(body, environment ++ arguments.map { case (k, v) => (k, `oType->type`(v)) }, subst)
-
-        new Answer(FunctionType(arguments.map(_._2).toList, retType), subst)
-      }
-      case Apply(lambda, parameters) => ???
-      case ValDecl(variable, body, oVarTypes, oReturnType) => {
-        val variableTypes = variable.map { case (k, _) => k -> `oType->type`(oVarTypes.get(k).getOrElse(null)) }
-        val resultType = `oType->type`(oReturnType)
-
-        val f = new FunctionType(variableTypes.values.toList, resultType)
-        val ans = typeOf(body, environment ++ variableTypes, subst)
-
-        unifier(ans.ty, resultType, subst, body);
-
-        typeOf(body, environment ++ variable.map { case (k: Val, v: Expression) => (k, typeOf(v, environment, subst).ty) }, subst)
-      }
-    }
+    new Answer(FunctionType(args.values.toList, retType), ans.subst)
   }
 
-
-  private def evaluateBinaryOperation(l: Expression, operation: Operation, r: Expression, subst: Map[Type, VarType], environment: Map[Expression, Type]) = {
+  private def evaluateBinaryOperation(l: Expression, operation: Operation, r: Expression, subst: Map[VarType, Type], environment: Map[Expression, Type]) = {
     val ans1 = typeOf(l, environment, subst)
-    unifier(ans1.ty, new IntType, ans1.subst, l)
+    val subst1 = unifier(ans1.ty, new IntType, ans1.subst, l)
 
-    val ans2 = typeOf(r, environment, ans1.subst)
-    unifier(ans2.ty, new IntType, ans2.subst, r)
+    val ans2 = typeOf(r, environment, subst1)
+    val subst2 = unifier(ans2.ty, new IntType, ans2.subst, r)
 
     operation match {
-      case _: IntOperation => Answer(new IntType, ans2.subst)
-      case _: BoolOperation => Answer(new BoolType, ans2.subst)
+      case _: IntOperation => Answer(new IntType, subst2)
+      case _: BoolOperation => Answer(new BoolType, subst2)
     }
   }
 
@@ -169,23 +133,27 @@ object Checker {
     }
   }
 
-  def typeToExternalForm(ty: Type): String = {
+  def typeToExternalForm(ty: Type, subst: Map[VarType, Type] = Map()): String = {
     ty match {
       case IntType() => "int"
       case BoolType() => "bool"
-      case FunctionType(argumentsTypes, resultType) => argumentsTypes.map(argType => typeToExternalForm(argType)).mkString(", ") + " -> " + typeToExternalForm(resultType)
-      case VarType(sn) => sn
+      case FunctionType(argumentsTypes, resultType) => argumentsTypes.map(argType => typeToExternalForm(argType, subst)).mkString(", ") + " -> " + typeToExternalForm(resultType, subst)
+      case self@VarType(_) => subst.get(self) match {
+        case Some(value) => typeToExternalForm(value)
+        case None => self.sn
+      }
     }
   }
 
   object freshTvarType {
-    val counter = 0
+    var counter = 0
 
     def getType(): VarType = {
-      new VarType(s"t${counter + counter + 1}")
+      counter += 1
+      new VarType(s"t${counter}")
     }
   }
 
-  case class Answer(ty: Type, subst: Map[Type, VarType] = Map())
+  case class Answer(ty: Type, subst: Map[VarType, Type] = Map())
 
 }
--- hw02-type-inference/src/main/scala/Source.scala
+++ hw02-type-inference/src/main/scala/Source.scala
@@ -5,13 +5,12 @@ object Source extends App {
 
   //  BinaryOperationsTest
   //  IfTest
-  //  LambdaTest
+  LambdaTest
   //  valDeclTest
   //  applyTest
   //  factorialTest
-  recTest
+  //  recTest
 
-  //  Console.println(Checker.typeOf(ValDecl(Map(Val("x") -> BinaryOperation(Const(3), Add, Const(4))), BinaryOperation(Const(2), Add, Val("x")))))
 
   private def recTest = {
     println(Checker.typeToExternalForm(Checker.typeOf(ValDecl(
@@ -29,55 +28,56 @@ object Source extends App {
           BinaryOperation(Val("n"), Mul, Apply(Val("fact"), Map(Val("n") -> BinaryOperation(Val("n"), Sub, Const(1))))))
       })), Apply(Val("fact"), Map(Val("n") -> Const(5)))
     )
-    Console.println(Checker.apply(fact5))
+    println(Checker.typeToExternalForm(Checker.typeOf(fact5).ty))
   }
 
   private def applyTest = {
     val lambdaBO = Lambda(Map(Val("n") -> new IntType), BinaryOperation(Val("n"), Add, Const(2)))
-    Console.println(Checker.apply(Apply(lambdaBO, Map(Val("n") -> BinaryOperation(Val("x"), Mul, Val("y")))), Map(Val("x") -> new IntType, Val("y") -> new IntType)))
+    println(Checker.typeToExternalForm(Checker.typeOf(Apply(lambdaBO, Map(Val("n") -> BinaryOperation(Val("x"), Mul, Val("y")))), Map(Val("x") -> new IntType, Val("y") -> new IntType)).ty))
 
     val lambdaIf = Lambda(Map(Val("n") -> new IntType, Val("y") -> new IntType), If(BinaryOperation(Const(1), Eq, Val("n")), Val("y"), Const(321)))
-    Console.println(Checker.apply(Apply(lambdaIf, Map(Val("n") -> Const(1), Val("y") -> Const(123)))))
+    println(Checker.typeToExternalForm(Checker.typeOf(Apply(lambdaIf, Map(Val("n") -> Const(1), Val("y") -> Const(123)))).ty))
   }
 
   private def valDeclTest = {
-    Console.println(Checker.apply(ValDecl(Map(Val("x") -> BinaryOperation(Const(3), Add, Const(4))), BinaryOperation(Const(2), Add, Val("x")))))
-    Console.println(Checker.apply(ValDecl(Map(Val("x") -> BinaryOperation(Const(3), Add, Const(4))), BinaryOperation(Const(2), Eq, Val("x")))))
+    println(Checker.typeToExternalForm(Checker.typeOf(ValDecl(Map(Val("x") -> BinaryOperation(Const(3), Add, Const(4))), BinaryOperation(Const(2), Add, Val("x")))).ty))
+    println(Checker.typeToExternalForm(Checker.typeOf(ValDecl(Map(Val("x") -> BinaryOperation(Const(3), Add, Const(4))), BinaryOperation(Const(2), Eq, Val("x")))).ty))
 
     try {
       val valDecl = ValDecl(Map(
         Val("x") -> BinaryOperation(Const(3), Add, Const(4)),
         Val("y") -> Lambda(Map(Val("n") -> new IntType, Val("z") -> new IntType), BinaryOperation(Val("n"), Add, Val("z")))), BinaryOperation(Val("y"), Add, Val("x")))
-      Console.println(Checker.apply(valDecl))
+      println(Checker.typeToExternalForm(Checker.typeOf(valDecl).ty))
     } catch {
       case e: Exception => println(e)
     }
   }
 
   private def LambdaTest = {
-    try {
-      println(Checker.typeToExternalForm(Checker.typeOf(Lambda(Map(Val("n") -> new IntType, Val("x") -> new BoolType), BinaryOperation(Val("n"), Add, Val("x")))).ty))
-    } catch {
-      case e: Exception => println(e)
-    }
-    try {
-      println(Checker.typeToExternalForm(Checker.typeOf(Lambda(Map(Val("n") -> new IntType, Val("x") -> new IntType), BinaryOperation(Val("n"), Add, BinaryOperation(Val("n"), Eq, Val("x"))))).ty))
-    } catch {
-      case e: Exception => println(e)
-    }
-    println(Checker.typeToExternalForm(Checker.typeOf(Lambda(Map(Val("n") -> new IntType, Val("x") -> new IntType), BinaryOperation(Val("n"), Add, Val("x")))).ty))
-    println(Checker.typeToExternalForm(Checker.typeOf(Lambda(Map(Val("n") -> new IntType, Val("x") -> new IntType), BinaryOperation(Val("n"), Add, BinaryOperation(Val("n"), Sub, Val("x"))))).ty))
-    println(Checker.typeToExternalForm(Checker.typeOf(Lambda(Map(Val("n") -> new IntType), Val("n"))).ty))
+    //      val ans = Checker.typeOf(Lambda(Map(Val("n") -> new NoType, Val("x") -> new NoType), BinaryOperation(Val("n"), Add, Val("x"))))
+    //      println(Checker.typeToExternalForm(ans.ty, ans.subst))
+
+    val ans = Checker.typeOf(Lambda(Map(Val("x") -> new NoType, Val("y") -> new NoType, Val("z") -> new NoType), If(Val("x"), Val("y"), Val("z"))))
+    println(Checker.typeToExternalForm(ans.ty, ans.subst))
+
+    //    try {
+    //      println(Checker.typeToExternalForm(Checker.typeOf(Lambda(Map(Val("n") -> new NoType, Val("x") -> new NoType), BinaryOperation(Val("n"), Add, BinaryOperation(Val("n"), Eq, Val("x"))))).ty))
+    //    } catch {
+    //      case e: Exception => println(e)
+    //    }
+    //    println(Checker.typeToExternalForm(Checker.typeOf(Lambda(Map(Val("n") -> new NoType, Val("x") -> new NoType), BinaryOperation(Val("n"), Add, Val("x")))).ty))
+    //    println(Checker.typeToExternalForm(Checker.typeOf(Lambda(Map(Val("n") -> new NoType, Val("x") -> new NoType), BinaryOperation(Val("n"), Add, BinaryOperation(Val("n"), Sub, Val("x"))))).ty))
+    //    println(Checker.typeToExternalForm(Checker.typeOf(Lambda(Map(Val("n") -> new NoType), Val("n"))).ty))
   }
 
   private def BinaryOperationsTest = {
     try {
-      println(Checker.apply(BinaryOperation(BinaryOperation(Bool(true), Sub, Const(1)), Add, Const(2))))
+      println(Checker.typeToExternalForm(Checker.typeOf(BinaryOperation(BinaryOperation(Bool(true), Sub, Const(1)), Add, Const(2))).ty))
     } catch {
       case e: Exception => println(e)
     }
     try {
-      Checker.apply(If(BinaryOperation(Const(1), Eq, Const(1)), Const(1), Bool(true)))
+      println(Checker.typeToExternalForm(Checker.typeOf(If(BinaryOperation(Const(1), Eq, Const(1)), Const(1), Bool(true))).ty))
     } catch {
       case e: Exception => println(e)
     }
@@ -86,12 +86,12 @@ object Source extends App {
 
   private def IfTest = {
     try {
-      println(Checker.apply(If(BinaryOperation(Const(1), Add, Const(1)), Const(1), Const(2))))
+      println(Checker.typeToExternalForm(Checker.typeOf(If(BinaryOperation(Const(1), Add, Const(1)), Const(1), Const(2))).ty))
     } catch {
       case e: Exception => println(e)
     }
-    println(Checker.apply(If(BinaryOperation(Const(1), Eq, Const(1)), Const(1), Const(2))))
-    println(Checker.apply(If(BinaryOperation(Const(1), Eq, Const(1)), Bool(true), Bool(false))))
-    println(Checker.apply(If(BinaryOperation(Const(1), Eq, BinaryOperation(Const(1), Add, Const(0))), Const(1), Const(2))))
+    println(Checker.typeToExternalForm(Checker.typeOf(If(BinaryOperation(Const(1), Eq, Const(1)), Const(1), Const(2))).ty))
+    println(Checker.typeToExternalForm(Checker.typeOf(If(BinaryOperation(Const(1), Eq, Const(1)), Bool(true), Bool(false))).ty))
+    println(Checker.typeToExternalForm(Checker.typeOf(If(BinaryOperation(Const(1), Eq, BinaryOperation(Const(1), Add, Const(0))), Const(1), Const(2))).ty))
   }
 }
